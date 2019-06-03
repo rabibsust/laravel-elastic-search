5895b512d306a676912cb5aa5359c3f75a0afc7f
Ahmed Jamaly AJ. Rabib <rabib@scbd.com>
2019-06-03 15:29:56 +0600

    Bind the interface in the app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Articles\ArticlesRepository;
+use App\Articles\EloquentArticlesRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ArticlesRepository::class, function () {
+            return new EloquentArticlesRepository();
+        });
     }
 
     /**
