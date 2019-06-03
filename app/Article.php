51477515b448ff12e641730798e582c3adf82164
Ahmad Jamaly Rabib <rabib.sust@gmail.com>
2019-06-03 16:13:26 +0600

    Added searchable trait to use in the articles class

--- app/Article.php
+++ app/Article.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Search\Searchable;
 
 class Article extends Model
 {
+    
+    use Searchable;
+    
     protected $casts = [
         'tags' => 'json',
     ];
